a256c4d1d3c72b512ebada47edb85d8319beff4f
prath0m <prathmeshklae902@gmail.com>
2025-09-08 16:58:30 +0530

    production ready

--- zerodha_app/settings.py
+++ zerodha_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bjamazjm3e(fooolwzya)g$d29%*^l=+d(lld7s$p$pd8^f5qx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.105.42.85']
 
 
 # Application definition
